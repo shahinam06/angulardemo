2adfa8ac7fda8b3b4bf8f950c0c4163b6753e61c
shahi <shahinam1606@hotmail.com>
2022-06-07 15:14:50 +0400

    Movie List -Shahina - 07/06/2022

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovieListComponent } from './movie-list/movie-list.component';
+import { MovieDescritionComponent } from './movie-description/movie-description.component';
+import { FormsModule } from '@angular/forms';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovieListComponent,
+    MovieDescritionComponent,
+   
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    Ng2SearchPipeModule,
     AppRoutingModule
   ],
   providers: [],
